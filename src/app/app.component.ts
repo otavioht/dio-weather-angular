1008523cd3206ccc07f06def52f1c552b1f39eb5
Otavio <otaviotsukamoto@gmail.com>
2021-03-31 14:09:19 -0300

    Upload do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import * as moment from 'moment-timezone';
+import 'moment/locale/pt-br';
 @Component({
   selector: 'dio-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dio-weather-angular';
+  constructor(){
+    moment.locale('pt-br');
+  }
 }
